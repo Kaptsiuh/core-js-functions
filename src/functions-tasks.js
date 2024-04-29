cfca36fc932dd8d799a20dd8182135d574926643
Dima Berry <dimaberry1@gmail.com>
2024-04-30 01:07:51 +0200

    feat: solve memoizes passed function and returns function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -109,8 +109,14 @@ function getPolynom(...arg) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+  return () => {
+    if (result === undefined) {
+      result = func();
+    }
+    return result;
+  };
 }
 
 /**
